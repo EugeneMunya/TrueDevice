1b78bae9b86f7da6839577487d3e69f1c71cc884
EugeneMunya <eugenemu57@gmail.com>
2021-01-10 11:15:44 +0300

    Create retrofit client and api interface function

--- app/src/main/java/com/example/truedevice/ui/auth/RegistrationFragment.kt
+++ app/src/main/java/com/example/truedevice/ui/auth/RegistrationFragment.kt
@@ -1,60 +1,50 @@
 package com.example.truedevice.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
+import android.util.Log.DEBUG
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.graphics.alpha
+import androidx.fragment.app.Fragment
 import com.example.truedevice.R
+import com.example.truedevice.databinding.FragmentRegistrationBinding
+import com.hbb20.CountryCodePicker
+import java.util.logging.Logger
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegistrationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegistrationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    var ccp: CountryCodePicker? = null
+   var editTextCarrierNumber: EditText? = null
+    lateinit var binding:FragmentRegistrationBinding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+        binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        ccp = binding.ccp.findViewById(R.id.ccp) as CountryCodePicker
+        editTextCarrierNumber= binding.editTextCarrierNumber.findViewById(R.id.editText_carrierNumber) as EditText
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegistrationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistrationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.signupBtn.setOnClickListener {
+            ccp?.registerCarrierNumberEditText(editTextCarrierNumber)
+            if (ccp?.isValidFullNumber() == true){
+                Toast.makeText(requireContext(), ccp?.fullNumberWithPlus.toString(), Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(requireContext(), "Your number is not valid", Toast.LENGTH_SHORT).show()
             }
+
+        }
+
     }
+
+
 }
