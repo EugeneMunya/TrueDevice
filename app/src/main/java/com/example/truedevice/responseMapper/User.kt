d02e8913f1bf84d4e112eafa6b42c0025d0910d2
EugeneMunya <eugenemu57@gmail.com>
2021-01-10 11:09:02 +0300

    Create retrofit client and api interface function

---
+++ app/src/main/java/com/example/truedevice/responseMapper/User.kt
@@ -0,0 +1,10 @@
+package com.example.truedevice.responseMapper
+
+data class User(
+        val firstName:String,
+        val lastName:String,
+        val email:String,
+        val phoneNumber:Number,
+        val nID:Number,
+        val access_token:String
+)
